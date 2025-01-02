8a3eae62bd9a9c9cc31d4a827e702e19cbbfefe0
Nguyễn Trọng Hiếu <tronghieu220403@gmail.com>
2025-01-02 11:38:05 +0700

    Clean unused libraries

--- EtwService/include/ulti/support.h
+++ EtwService/include/ulti/support.h
@@ -4,30 +4,15 @@
 
 #ifndef ETWSERVICE_ULTI_SUPPORT_H_
 #define ETWSERVICE_ULTI_SUPPORT_H_
-
-#include <string.h>
-#include <time.h>
-#include <WinSock2.h>
+#include <Windows.h>
 #include <iphlpapi.h>
-#include <WS2tcpip.h>
 #include <TlHelp32.h>
 #include <Psapi.h>
-#include <direct.h>
-#include <stdio.h>
-#include <conio.h>
-#include <strsafe.h>
-#include <tchar.h>
-#include <locale.h>
-#include <Windows.h>
-#include <Shlobj.h>
-#include <Pdh.h>
-#include <PdhMsg.h>
 #include <wmistr.h>
 #include <evntrace.h>
 #include <evntcons.h>
 #include <comutil.h>
 #include <wbemidl.h>
-#include <aclapi.h>    
 #include <winternl.h>
 
 #include <memory>
@@ -57,12 +42,8 @@
 #include <cctype>
 #include <codecvt>
 
-#pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "iphlpapi.lib")
-#pragma comment(lib, "Shell32.lib") 
 #pragma comment(lib, "comsupp.lib")
-#pragma comment(lib, "Pdh.lib")
-#pragma comment(lib, "Advapi32.lib")
 #pragma comment(lib, "ntdll.lib")
 
 #pragma warning(disable : 26110)
